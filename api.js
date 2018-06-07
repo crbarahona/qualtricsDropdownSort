f5b69d7409a5aa8cf3d1d7e7b3aedf8799c25314
Carlos Barahona <crbarahona@ucdavis.edu>
2018-06-06 23:22:19 -0700

    Resolved JFE6 issues adding and removing from box.

--- api.js
+++ api.js
@@ -1,8 +1,21 @@
 Qualtrics.SurveyEngine.addOnload(function()
 {
+	/*Place your JavaScript here to run when the page loads*/
+
+});
+
+Qualtrics.SurveyEngine.addOnload(function()
+{
+
+	function jq( myid ) {
+
+    return "#" + myid.replace( /(:|\.|\[|\]|\~|,|=|@)/g, "\\$1" );
+
+	}
+
 	if(typeof(QSettings) != 'undefined'){
   // Get Complete items list for dropdown
-  var itemsList = $j("#QID2items li,#QID2group0 li").sort(function (a, b) {
+  var itemsList = $j("#QID18items li,#QID18group0 li").sort(function (a, b) {
     if(parseInt(a.id.split("~")[2]) > parseInt(b.id.split("~")[2]))
         return 1;
     else
@@ -12,16 +25,16 @@ Qualtrics.SurveyEngine.addOnload(function()
   var itemsArr = [];
 
   //Add removes glyphs to existing selected counties
-  $j($("QID2group0")).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
+  $j($(jq("QID18group0"))).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
   //Add click listener to remove
-  $j($("QID2group0")).find("label span.glyphicon-remove").click(function(e){
+  $j($(jq("QID18group0"))).find("label span.glyphicon-remove").click(function(e){
     e.preventDefault();
     var item = this.parentNode.parentNode.parentNode.parentNode.parentNode;
-    $("QID2items").appendChild($(item.id));
+    $(jq("QID18items")).append($(jq(item.id)));
     $j(item).find(".glyphicon").remove();
     $j("#properties option[value='" + item.id +"']").prop("disabled", false);
 	var choiceId = item.id.split("~")[2]
-	var choice = Qualtrics.SurveyEngine.getInstance("QID2").question.runtime.Choices[choiceId];
+	var choice = Qualtrics.SurveyEngine.getInstance("QID18").question.runtime.Choices[choiceId];
 	choice.Value = null;
 	choice.Group = null;
 	choice.Selected = false;
@@ -33,7 +46,7 @@ Qualtrics.SurveyEngine.addOnload(function()
     itemsArr[i] = {"id": id, "name": name };
   }
   $j(".Items ul").hide();
-  $j("#QID2 .Items h2").text("Counties")
+  $j("#QID18 .Items h2").text("Sectors")
   $j(".Items").append($j('<select></select>').attr("id","properties").addClass("form-control"));
   $j(".Items").append($j('<button>Add Selected</button>').addClass("btn btn-primary").attr("type","button"));
   $j("#properties").append($j("<option value=''></option>"));
@@ -45,49 +58,50 @@ Qualtrics.SurveyEngine.addOnload(function()
       .text(value.name));
   });
 
-  $j("#QID2group0 li").each(function(a,b){
+  $j("#QID18group0 li").each(function(a,b){
     $j("#properties option[value='" + b.id +"']").prop("disabled", true);
   });
   $j("button").click(function(e){
+		debugger;
     e.preventDefault();
-	choice = Qualtrics.SurveyEngine.getInstance("QID2").question.runtime.Choices[$j($($("properties").value)).attr("data-choiceid")];
+	choice = Qualtrics.SurveyEngine.getInstance("QID18").question.runtime.Choices[$j($(jq($(jq("properties")).val()))).attr("data-choiceid")];
 	choice.Value = "" + 1;
 	choice.Group = "" + 0;
 	choice.Selected = true;
-    $("QID2group0").appendChild($($("properties").value));
-    $j($($("properties").value)).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
-    $j($($("properties").value)).find("label span.glyphicon-remove").click(function(e){
+    $(jq("QID18group0")).append($(jq($(jq("properties")).val())));
+    $j($(jq($(jq("properties")).val()))).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
+    $j($(jq($(jq("properties")).val()))).find("label span.glyphicon-remove").click(function(e){
       e.preventDefault();
     var item = this.parentNode.parentNode.parentNode.parentNode.parentNode;
-      $("QID2items").appendChild($(item.id));
+      $(jq("QID18items")).append($(item.id));
       $j(item).find(".glyphicon").remove();
       $j("#properties option[value='" + item.id +"']").prop("disabled", false);
       var choiceId = item.id.split("~")[2]
-	  var choice = Qualtrics.SurveyEngine.getInstance("QID2").question.runtime.Choices[choiceId];
+	  var choice = Qualtrics.SurveyEngine.getInstance("QID18").question.runtime.Choices[choiceId];
 	  choice.Value = null;
 	  choice.Group = null;
 	  choice.Selected = false;
       });
-    $j("#properties option[value='" + $("properties").value +"']").prop("disabled", true);
+    $j("#properties option[value='" + $(jq("properties")).value +"']").prop("disabled", true);
     $j("#properties option[value='']").attr('selected', true);
   });
 }
 	else{
-  var itemsList = $j("#QID2items li,#QID2group0 li").sort(function (a, b) {
+  var itemsList = $j("#QID18items li,#QID18group0 li").sort(function (a, b) {
     if(parseInt(a.id.split("~")[2]) > parseInt(b.id.split("~")[2]))
         return 1;
     else
         return -1
   });
   var itemsArr = [];
-  $j($("QID2group0")).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
-  $j($("QID2group0")).find("label span.glyphicon-remove").click(function(e){
+  $j($(jq("QID18group0"))).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
+  $j($(jq("QID18group0"))).find("label span.glyphicon-remove").click(function(e){
     e.preventDefault(); debugger;
     var item = this.parentNode.parentNode.parentNode.parentNode;
-    $("QID2items").appendChild($(item.id));
+    $(jq("QID18items")).append($(item.id));
     $j(item).find(".glyphicon").remove();
     $j("#properties option[value='" + item.id +"']").prop("disabled", false);
-    updateInputValues(["QID2group0","QID2items"]);
+    updateInputValues(["QID18group0","QID18items"]);
     });
   for(var i=0; i < itemsList.length; i++){
     var item = itemsList[i];
@@ -96,7 +110,7 @@ Qualtrics.SurveyEngine.addOnload(function()
     itemsArr[i] = {"id": id, "name": name };
   }
   $j(".Items ul").hide();
-  $j("#QID2 .Items h2").text("Counties")
+  $j("#QID18 .Items h2").text("Counties")
   $j(".Items").append($j('<select></select>').attr("id","properties").addClass("form-control"));
   $j(".Items").append($j('<button>Add Selected</button>').addClass("btn btn-primary").attr("type","button"));
   $j("#properties").append($j("<option value=''></option>"));
@@ -108,24 +122,31 @@ Qualtrics.SurveyEngine.addOnload(function()
       .text(value.name));
   });
 
-  $j("#QID2group0 li").each(function(a,b){
+  $j("#QID18group0 li").each(function(a,b){
     $j("#properties option[value='" + b.id +"']").prop("disabled", true);
   });
   $j("button").click(function(e){
     e.preventDefault();
-    $("QID2group0").appendChild($($("properties").value));
-    $j($($("properties").value)).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
-    $j($($("properties").value)).find("label span.glyphicon-remove").click(function(e){
+    $("QID18group0").append($($(jq("properties")).val()));
+    $j($($(jq("properties")).val())).find("label").append($j("<span></span>").addClass("glyphicon glyphicon-remove"));
+    $j($($(jq("properties")).val())).find("label span.glyphicon-remove").click(function(e){
       e.preventDefault();
       var item = this.parentNode.parentNode.parentNode.parentNode;
-      $("QID2items").appendChild($(item.id));
+      $(jq("QID18items")).append($(item.id));
       $j(item).find(".glyphicon").remove();
       $j("#properties option[value='" + item.id +"']").prop("disabled", false);
-      updateInputValues(["QID2group0","QID2items"]);
+      updateInputValues(["QID18group0","QID18items"]);
       });
-    updateInputValues("QID2group0");
-    $j("#properties option[value='" + $("properties").value +"']").prop("disabled", true);
+    updateInputValues("QID18group0");
+    $j("#properties option[value='" + $(jq("properties")).value +"']").prop("disabled", true);
     $j("#properties option[value='']").attr('selected', true);
   });
 	}
+});
+
+
+Qualtrics.SurveyEngine.addOnUnload(function()
+{
+	/*Place your JavaScript here to run when the page is unloaded*/
+
 });
